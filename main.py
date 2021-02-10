ad511a0868c635b7a6272902618ff6a2e6c4a4f6
Derek Belrose <derek@derekbelrose.com>
2021-02-10 14:03:43 -0500

    LDAP query works, first we gather github users then ldap

--- main.py
+++ main.py
@@ -1,9 +1,85 @@
 import requests
 import os
+import ldap
 
 token = os.environ['GITHUB_TOKEN']
 headers = {"Authorization": f"Bearer {token}"}
+ldap_password = os.environ['JHED_PASSWORD']
+ldap_username = os.environ['JHED_ID']
 
+class GitHubUser(object):
+    login = None
+    own_repositories = []
+    organizations = []
+    
+    
+class JHEDUser(object):
+    jhed_id = None
+    givenName = None
+    surName = None
+    displayName = None
+    role = None
+    group = None
+    title = None
+    mail = None
+    department = None
+    
+class User(GitHubUser, JHEDUser):
+    def init(self):
+        self.login = ''
+        self.jhed_id = ''
+        self.givenName = ''
+        self.surName = ''
+        self.displayName = ''
+        self.role = ''
+        self.group = ''
+        self.title = ''
+        self.mail = ''
+        self.department = ''
+
+    def gh_populate(self, blob):
+        self.jhed_id = blob['jhed_id'].split('@')[0]
+        self.login = blob['gh_login']
+        
+    def __repr__(self):
+        return f'''
+JHED Id: {self.jhed_id}
+GitHub Login: {self.login}
+Name: {self.displayName}
+Mail: {self.mail}
+Title: {self.title}
+Role: {self.role}
+Department: {self.department}
+Group: {self.group}'''
+
+
+def get_jhed_user(user):
+
+    l = ldap.initialize('ldaps://ldap.johnshopkins.edu:636', bytes_mode=False)
+    username = "cn={0},ou=people,dc=win,dc=ad,dc=jhu,dc=edu".format(ldap_username)
+    try:
+        l.protocol_version = ldap.VERSION3
+        l.simple_bind_s(username, ldap_password)
+        valid = True
+
+        r = l.search_s('ou=people,dc=win,dc=ad,dc=jhu,dc=edu', ldap.SCOPE_SUBTREE, '(cn={0})'.format(user.jhed_id))
+
+        obj = r[0][1]
+        user.givenName = str(obj['givenName'][0], 'ASCII')
+        user.surName = str(obj['sn'][0], 'ASCII')
+        user.displayName = str(obj['displayName'][0], 'ASCII')
+        user.role = str(obj['eduPersonAffiliation'][0], 'ASCII')
+        user.group = str(obj['ou'][0], 'ASCII')
+        user.title = str(obj['title'][0], 'ASCII')
+        user.mail = str(obj['mail'][0], 'ASCII')
+        user.department = str(obj['eduPersonOrgUnitDn'][0], 'ASCII')
+
+        return user
+
+    except Exception as error:
+        print(error)
+        exit
+    
 def run_query(query, variables):
     request = requests.post('https://api.github.com/graphql', json={'query': query, 'variables': variables}, headers=headers)
 
@@ -58,6 +134,9 @@ def get_samlidentities(slug, cursor=None):
 
     return identities
 
-print("My GitHub ID is:,JHED ID")
 for ident in get_samlidentities("johns-hopkins-university"):
-    print(f"{ident['gh_login']},{ident['jhed_id']}")
+    user = User()
+    user.gh_populate(ident)
+
+    user = get_jhed_user(user)
+    print(user)
